b13e840a1e35dc82fe6962a730ac379104ac6287
dourgey <dourgey@gmail.com>
2023-08-30 03:41:48 +0800

    从文件读取改为从sqlite读取

--- modules/dataset.py
+++ modules/dataset.py
@@ -2,44 +2,33 @@ import json
 import os
 from pathlib import Path
 import tqdm
+import sqlite3
 from torch.utils.data import Dataset, DataLoader
 
 class BreadMidiDataset(Dataset):
-    def __init__(self, dir_path, max_dataset_length):
-        self.dir_path = Path(dir_path)
+    def __init__(self, db_path, max_dataset_length):
+        self.dir_path = Path(db_path)
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
 
-        file_list = {'output_file_1.txt': 34719, 'output_file_2.txt': 41621, 'output_file_3.txt': 41621,
-                     'output_file_4.txt': 41621, 'output_file_5.txt': 41621, 'output_file_6.txt': 41621,
-                     'output_file_7.txt': 41621, 'output_file_8.txt': 41621, 'output_file_9.txt': 41621,
-                     'output_file_10.txt': 41621, 'output_file_11.txt': 41621, 'output_file_12.txt': 41621,
-                     'output_file_13.txt': 41621, 'output_file_14.txt': 41621, 'output_file_15.txt': 41621,
-                     'output_file_16.txt': 41621, 'output_file_17.txt': 41621, 'output_file_18.txt': 41621,
-                     'output_file_19.txt': 41621, 'output_file_20.txt': 41621}
+        self.max_len = max_dataset_length
 
-        self.file_list = {x: file_list[x] for x in file_list}
 
-        assert os.path.exists(self.dir_path)
-        for x in self.file_list:
-            assert os.path.exists(self.dir_path / x)
-
-        self.length = min(max_dataset_length, sum(self.file_list.values()))
+        if max_dataset_length in [1024, 2048, 4096, 8192, 16384, 32768, 8192]:
+            kv = {1024: 106971, 2048: 172704, 4096: 278835, 8192: 433837, 16384: 672644, 32768: 809395}
+            self.max_idx = kv[max_dataset_length]
+        else:
+            self.max_idx = self.cursor.execute("select count(id) from midi_pretrain_dataset where token_length <= ?",
+                                               (self.max_len,)).fetchone()[0]
 
 
 
     def __getitem__(self, index):
-        count = 0
-        for i in range(1, 21):
-            if count + self.file_list[f"output_file_{i}.txt"] < index:
-                count += self.file_list[f"output_file_{i}.txt"]
-            else:
-                break
-        line = self._read_specific_line(self.dir_path / f"output_file_{i}.txt", index - count)
-
-        return json.loads(line)["sentence"]
+        return self.cursor.execute("""select sentence from midi_pretrain_dataset where id = ?""", (index,)).fetchone()[0]
 
 
     def __len__(self):
-        return self.length
+        return self.max_idx
 
     @staticmethod
     def _read_specific_line(filename, index):
@@ -64,15 +53,19 @@ class BreadMidiDataset(Dataset):
 class MidiCollator:
 
     def __init__(self, tokenizer):
-        pass
+        self.tokenizer = tokenizer
 
     def __call__(self, batch):
         pass
 
 
 if __name__ == '__main__':
-    dataset = BreadMidiDataset("../data/training_data", 100)
+
+    dataset = BreadMidiDataset(r"C:\Users\zhaoj\Desktop\sqlite\dataset.db", 4096)
+    print(len(dataset))
     dataloader = DataLoader(dataset, batch_size=24, shuffle=False, num_workers=0, collate_fn=None)
     for data in tqdm.tqdm(dataloader):
         print(data)
+        break
+
 
